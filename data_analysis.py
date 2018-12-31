0be8f6711fe83f8ef54f248e55895bcb31f55576
skim2257 <40668167+skim2257@users.noreply.github.com>
2018-12-31 05:47:36 -0600

    Update data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -4,14 +4,6 @@ import matplotlib.pyplot as plt
 from itertools import count
 import matplotlib.font_manager
 
-# def selectPG(data, peopleGroup):
-#     # Output Features:
-#     #    - Country Name
-#     #    - 2030 Mortality Climate Total
-#     #    - Country-specific People Group's Population
-#     return data.loc[lambda data: data.PeopNameAcrossCountries == peopleGroup, :][['Country',key, 'Population']]
-
-
 def weighted_avg_and_std(values, weights):
     """
     Return the weighted average and standard deviation.
@@ -23,10 +15,8 @@ def weighted_avg_and_std(values, weights):
     variance = np.average((values-average)**2, weights=weights)
     return (average, np.sqrt(variance))
 
-
 def calc_country_pop(country):
     return np.sum(df.loc[df['Country'] == country]['Population'].values)
-
 
 def normalize(df, feature_name):
     result = df.copy()
@@ -56,13 +46,6 @@ df = normalize(df, 'risk')
 
 key = 'risk'
 
-# col_headers = list(df)
-# col_headers
-
-# people_groups = df['PeopNameAcrossCountries'].unique()
-# people_groups.sort()
-# people_groups.shape
-
 subdf = df.loc[df['Country'].isin(['India'])]
 
 pg_list = subdf['PeopNameAcrossCountries'].values
@@ -73,39 +56,6 @@ for pg in pg_list:
     pg_set = pg_set.union(temp_pg_set)
 pgs = list(pg_set)
 pgs.sort()
-
-
-# test_pg = people_groups[3]
-# test_pg
-
-# subdf = df.loc[df['PeopNameAcrossCountries'].str.contains('test_pg')]
-# subdf
-
-# subdf[[key, 'Population']]
-
-# x = subdf[key].values
-# weights = subdf['Population'].values
-# x, weights
-
-# avg, std = weighted_avg_and_std(x, weights)
-# avg, std
-
-# import matplotlib.pyplot as plt
-# n, bins, patches = plt.hist(x, weights=weights)
-# bin_centers = 0.5 * (bins[:-1] + bins[1:])
-# 
-# # This is  the colormap I'd like to use.
-# cm = plt.cm.get_cmap('RdYlBu_r')
-# 
-# # scale values to interval [0,1]
-# col = bin_centers - min(bin_centers)
-# col /= max(col)
-# 
-# for c, p in zip(col, patches):
-#     plt.setp(p, 'facecolor', cm(c))
-# 
-# plt.axvline(x=avg, color='k')
-# plt.errorbar(x=avg, y=max(n), xerr=std, ecolor='k', capsize=10)
 
 cm = plt.cm.get_cmap('RdYlBu_r')
 all_avg, all_std = weighted_avg_and_std(df[key].values, df['Population'].values)
@@ -118,21 +68,9 @@ for pg in pgs:
         continue
     elif subdf[[key, 'Population']].isnull().any().any():
         continue
-
-    # print(subdf[['Country', key, 'Population']])
-    # x = np.empty(len(subdf))
-    # for i, country, mortality_rate in zip(count(), subdf['Country'].values, subdf[key].values):
-    #     x[i] = mortality_rate
-    #     country_pop = calc_country_pop(country)
-    #     x[i] /= country_pop
-        # x[i] *= 1000
-        # if x[i] > 1:
-        #     print('problem')
     x = subdf[key].values
     weights = subdf['Population'].values
-    # print(x, weights)
     avg, std = weighted_avg_and_std(x, weights)
-    # print(avg, std)
 
     fig = plt.figure()
     n, bins, patches = plt.hist(x, weights=weights, range=(min(df[key]), max(df[key])), align='mid')
